ad32cd3eaec6bbd356d22d56532ecae861ed3dcf
Youhana-Hana <meyouhana@gmail,com>
2013-08-13 22:28:38 +0100

    fix image picker && add confirmation before clear

--- src/mobi/MobiSeeker/sPromotions/activites/NewPromotion.java
+++ src/mobi/MobiSeeker/sPromotions/activites/NewPromotion.java
@@ -17,31 +17,32 @@ import android.widget.ImageView;
 
 public class NewPromotion extends Fragment {
 
-    Button entrySave = null;
-    Button entrySaveCancel = null;
-    EditText  entryTitle = null;
-    EditText  entrySummary = null;
-    ImageView  entryImage = null;
-    Entry entryFromIntent = null;
-    String nodeName = null;
-  
-    public void setNodeName(String nodeName) {
-    	this.nodeName = nodeName;
-    }
-    
- 	@Override
+	Button entrySave = null;
+	Button entrySaveCancel = null;
+	EditText entryTitle = null;
+	EditText entrySummary = null;
+	ImageView entryImage = null;
+	Entry entryFromIntent = null;
+	String nodeName = null;
+
+	public void setNodeName(String nodeName) {
+		this.nodeName = nodeName;
+	}
+
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(
-				R.layout.newpromotion, container, false);
-	
+		View rootView = inflater.inflate(R.layout.newpromotion, container,
+				false);
+
 		this.entryTitle = (EditText) rootView.findViewById(R.id.editTitle);
 		this.entrySummary = (EditText) rootView.findViewById(R.id.editText);
 		this.entryImage = (ImageView) rootView.findViewById(R.id.image);
-		
+
 		this.entrySave = (Button) rootView.findViewById(R.id.entrySave);
-		this.entrySaveCancel = (Button) rootView.findViewById(R.id.entrySaveCancel);
-	
+		this.entrySaveCancel = (Button) rootView
+				.findViewById(R.id.entrySaveCancel);
+
 		this.entrySave.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -49,71 +50,71 @@ public class NewPromotion extends Fragment {
 				goHome();
 			}
 		});
-		
+
 		this.entrySaveCancel.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				goHome();
 			}
 		});
-		
+
 		this.SetVauesFromIntent();
-		
+
 		return rootView;
 	}
-	
- 	private void SetVauesFromIntent() {
- 		Intent intent = getActivity().getIntent();
- 		Entry entry = (Entry)intent.getSerializableExtra("entry");
- 		if(entry == null) {
- 			return;
- 		}
- 		
- 		this.entryFromIntent = entry;
- 		
- 		this.entryTitle.setText(entry.getTitle());
- 		this.entrySummary.setText(entry.getText());
- 		String imageUrl = entry.getImagePath();
- 		if (imageUrl.isEmpty() || imageUrl == null) {
- 			return;
- 		}
- 		
- 		this.entryImage.setImageURI(Uri.parse(entry.getImagePath()));
- 	}
+
+	private void SetVauesFromIntent() {
+		Intent intent = getActivity().getIntent();
+		Entry entry = (Entry) intent.getSerializableExtra("entry");
+		if (entry == null) {
+			return;
+		}
+
+		this.entryFromIntent = entry;
+
+		this.entryTitle.setText(entry.getTitle());
+		this.entrySummary.setText(entry.getText());
+		String imageUrl = entry.getImagePath();
+		if (imageUrl.isEmpty() || imageUrl == null) {
+			return;
+		}
+
+		this.entryImage.setImageURI(Uri.parse(entry.getImagePath()));
+	}
 
 	void save() {
- 		Repository repository = new Repository(Promotions.Local);
- 		Context context = getActivity().getBaseContext();
- 		try {
+		Repository repository = new Repository(Promotions.Local);
+		Context context = getActivity().getBaseContext();
+		try {
 			repository.save(context, getEntry(context));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
- 	}
- 	
+	}
+
 	private Entry getEntry(Context context) {
-		
-		String  title = this.entryTitle.getText().toString();
-		String  content = this.entrySummary.getText().toString();
-		
+
+		String title = this.entryTitle.getText().toString();
+		String content = this.entrySummary.getText().toString();
+
 		String image = null;
-		if (this.entryImage.getTag() != null)
-		{
-			image = (String)this.entryImage.getTag();
+		if (this.entryImage.getTag() != null) {
+			image = (String) this.entryImage.getTag();
 		}
-		
-		String  logo = new mobi.MobiSeeker.sPromotions.data.Settings(context).getLogo();
-		
+
+		String logo = new mobi.MobiSeeker.sPromotions.data.Settings(context)
+				.getLogo();
+
 		if (this.entryFromIntent == null) {
 			return new Entry(title, content, logo, image, this.nodeName);
 		}
-		
+
 		this.entryFromIntent.setTitle(title);
 		this.entryFromIntent.setText(content);
 		this.entryFromIntent.setLogo(logo);
 		this.entryFromIntent.setImagePath(image);
-		return this.entryFromIntent; 
+		return this.entryFromIntent;
 	}
 
 	void goHome() {
--- src/mobi/MobiSeeker/sPromotions/activites/PromotionsList.java
+++ src/mobi/MobiSeeker/sPromotions/activites/PromotionsList.java
@@ -4,8 +4,10 @@ import mobi.MobiSeeker.sPromotions.R;
 import mobi.MobiSeeker.sPromotions.data.Adapter;
 import mobi.MobiSeeker.sPromotions.data.Entry;
 import mobi.MobiSeeker.sPromotions.data.Repository;
+import android.app.AlertDialog;
 import android.app.ListFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -41,13 +43,31 @@ public class PromotionsList extends ListFragment {
 		this.clear.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				clear();
+				confirmClear();
 			}
 		});
 
 		return rootView;
 	}
 
+	 public void confirmClear() {
+	     	AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+	        alertDialog.setTitle(this.getResources().getString(R.string.clear_alert_title));
+	        alertDialog.setMessage(this.getResources().getString(R.string.clear_alert_text));
+	        alertDialog.setPositiveButton(this.getResources().getString(android.R.string.yes), new DialogInterface.OnClickListener() {
+	            public void onClick(DialogInterface dialog, int which) {
+	                clear();
+	            }
+	        });
+
+	        alertDialog.setNegativeButton(this.getResources().getString(android.R.string.no), new DialogInterface.OnClickListener() {
+	            public void onClick(DialogInterface dialog, int which) {
+	            }
+	        });
+
+	        alertDialog.create().show();
+	    }
+	 
 	protected void clear() {
 		try {
 			this.repository.clear(getActivity().getBaseContext());
